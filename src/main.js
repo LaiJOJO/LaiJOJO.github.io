7f8e565b4b7da98d255e986e98388e710b8268db
带脑子的草履虫虫 <2485706183@qq.com>
2022-09-01 23:58:33 +0800

    crud基本开发完成

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import 'default-passive-events'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
